815d2f8924af387098b3daa8b4ce0f8105fdbb1f
Phil Leggetter <phil@leggetter.co.uk>
2024-01-19 19:35:37 +0000

    chore: add basic supabase video listing logic

--- components/RecordingsList.tsx
+++ components/RecordingsList.tsx
@@ -1,23 +1,29 @@
 import Image from "next/image";
 import { StreamClient } from "@stream-io/node-sdk";
+import { createClient as createClientUtil } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+type DisplayedRecording = {
+  url: string;
+  filename: string;
+  end_time: string;
+  title: string;
+};
 
 const serverClient = new StreamClient(
   process.env.NEXT_PUBLIC_STREAM_API_KEY!,
   process.env.STREAM_API_SECRET!
 );
 
-export default async function RecordingsList({ userId }: { userId: string }) {
+const getRecordingsFromStream = async (userId: string) => {
+  let recordings: DisplayedRecording[] = [];
+
   const response = await serverClient.video.queryCalls({
     filter_conditions: {
       created_by_user_id: { $eq: userId },
     },
   });
-  let recordings: {
-    url: string;
-    filename: string;
-    end_time: string;
-    title: string;
-  }[] = [];
+
   const callReponses = response?.calls;
   for (let i = 0; i < callReponses?.length; ++i) {
     const response = callReponses[i];
@@ -32,6 +38,41 @@ export default async function RecordingsList({ userId }: { userId: string }) {
       }))
     );
   }
+
+  return recordings;
+};
+
+const getRecordingsFromSupabase = async (userId: string) => {
+  const cookieStore = cookies();
+  const supabase = createClientUtil(cookieStore);
+
+  const { data, error } = await supabase
+    .from("videos")
+    .select("*")
+    .eq("user_id", userId);
+
+  const recordings: DisplayedRecording[] = [];
+  if (error) {
+    // TODO: change the logic here to handle the error
+    console.error(error);
+    return recordings;
+  }
+
+  data.forEach((video) => {
+    recordings.push({
+      url: video.url,
+      filename: video.url.substring(video.url.lastIndexOf("/") + 1),
+      end_time: video.created_at,
+      title: video.title,
+    });
+  });
+
+  return recordings;
+};
+
+export default async function RecordingsList({ userId }: { userId: string }) {
+  let recordings = await getRecordingsFromStream(userId);
+  // let recordings = await getRecordingsFromSupabase(userId);
 
   const MAX_TITLE_LENGTH = 60;
   const reduceTitle = (title: string) => {
@@ -49,7 +90,7 @@ export default async function RecordingsList({ userId }: { userId: string }) {
         </div>
       )}
 
-      {response && recordings.length > 0 && (
+      {recordings.length > 0 && (
         <ul className="w-full flex flex-wrap justify-center gap-10">
           {recordings.map((recording, index) => (
             <li
