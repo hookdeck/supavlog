7c9fc4c070109058964ab36ae02bdcd1aa4d1afb
Phil Leggetter <phil@leggetter.co.uk>
2024-01-26 15:03:48 +0000

    fix: build due to stream logo property

--- components/logos/StreamLogo.tsx
+++ components/logos/StreamLogo.tsx
@@ -3,9 +3,6 @@ export default function StreamLogo() {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlSpace="preserve"
-      style={{
-        enableBackground: "new 0 0 332.4 56",
-      }}
       viewBox="0 0 332.4 56"
       width="140"
       height="30"
